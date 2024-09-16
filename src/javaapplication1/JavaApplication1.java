e95167f73e9519613f12f6562da813edf48f6308
shivamkataria13 <shivamsinghkataria13@gmail.com>
2024-09-16 16:49:38 -0400

    Revert "second"
    This reverts commit 6f3d3dcb1f7f9c0236c20893dcef97d0b512d489.

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,6 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hey");
     }
     
 }
